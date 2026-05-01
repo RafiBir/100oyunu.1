c1b33d20438478cc7ab0a4ab2d01ab0cc8e72d43
RafiBir <rafybirmizrahi@gmail.com>
2026-05-01 16:43:09 +0300

    Bump cache for block bot mode

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'centium-v20';
+﻿const CACHE = 'centium-v21';
 const FILES = [
   './',
   './index.html',
@@ -29,3 +29,4 @@ self.addEventListener('fetch', e => {
     caches.match(e.request).then(cached => cached || fetch(e.request))
   );
 });
+
